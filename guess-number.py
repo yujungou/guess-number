87f9e7f6f01284add10ae1917a67ca058af2fbb1
Sherry Ou <sherrym711sr@gmail.com>
2023-06-24 10:52:16 +0800

    second version

--- guess-number.py
+++ guess-number.py
@@ -1,13 +1,17 @@
 import random
 
 r = random.randint(1,100)
+count = 0
 while True :
+	count = count + 1
 	rg = input('please guess a number: ')
 	rg = int(rg)
 	if rg == r:
 		print('You are right!')
+		print('This is your', count, 'try.')
 		break
 	elif rg > r:
 	    print('Your answer is bigger.') 
 	elif rg < r:
 		print('Your answer is smaller')
+	print('This is your', count, 'try.')
